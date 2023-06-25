3c7a60d5f4095127ddfff12618478d0ee39fe408
Suleiman Abdulkadir <sulea841@gmail.com>
2023-06-25 00:35:35 +0000

    section 8

--- section 8/index.js
+++ section 8/index.js
@@ -1,1 +1,8 @@
 const keys = require('./keys')
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+})
