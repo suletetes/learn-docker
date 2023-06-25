362cc74bb7d44190262a051c050e13f0b78e3027
Suleiman Abdulkadir <sulea841@gmail.com>
2023-06-25 17:34:01 +0000

    section 8

--- section 8/client/src/App.js
+++ section 8/client/src/App.js
@@ -22,6 +22,8 @@ function App() {
         >
           Learn React
         </a>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/otherpage"}>Other Page</Link>
       </header>
       <div>
         <Route exact path={"/"} component={fib}/>
