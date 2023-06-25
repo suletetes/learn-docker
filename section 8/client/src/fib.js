91b7a4c51c9424dc3b9b0b18424aa36a73f478af
Suleiman Abdulkadir <sulea841@gmail.com>
2023-06-25 17:07:02 +0000

    section 8

--- section 8/client/src/fib.js
+++ section 8/client/src/fib.js
@@ -24,6 +24,13 @@ class fib extends Component {
             seenIndexes: seenIndexes.data
         })
     }
+    handleSubmit = async (event)=>{
+        event.preventDefault();
+        await axios.post('/api/values', {
+            index: this.state.index
+        })
+        this.setState({ index: ''})
+    }
 
     renderSeenIndexes() {
         return this.state.seenIndexes.map(({number}) => number).join(', ')
@@ -38,14 +45,18 @@ class fib extends Component {
                 </div>
             )
         }
+        return entries
     }
 
     render() {
         return (
             <dv>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Enter your index:</label>
-                    <input/>
+                    <input
+                    value={this.state.index}
+                    onChange={event => this.setState({index: event.target.value})}
+                    />
                     <button>Submit</button>
                 </form>
                 <h3>Indexes i have seen:</h3>
