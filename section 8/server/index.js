7c3f4b03c7ab6db311571de23689226d02d2066b
Suleiman Abdulkadir <sulea841@gmail.com>
2023-06-25 14:44:04 +0000

    section 8

--- section 8/server/index.js
+++ section 8/server/index.js
@@ -48,3 +48,9 @@ app.get('/values/all', async(req, res) =>{
     const values = await  pgClient.query("SELECT * from values")
     res.send(values.rows)
 })
+
+app.get('/values/current', async(req, res)=>{
+    redisClient.hgetall('values', (err, values) =>{
+        res.send(values)
+    })
+})
