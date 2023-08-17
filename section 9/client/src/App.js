af348a5d94c2c9cd067bf10ea10605cd69296233
Suleiman <85356587+suletetes@users.noreply.github.com>
2023-08-17 00:09:06 +0000

    update

--- section 9/client/src/App.js
+++ section 9/client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Home fib calculator version 2</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
